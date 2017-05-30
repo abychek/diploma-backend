e74b6c5b8fb96be0e886e3792f754f6d3102995d
Alexey Bychek <alexey.bychek94@gmail.com>
2017-05-30 10:17:39 +0300

    Added get action for employees.

--- src/StaffBundle/Controller/EmployeeController.php
+++ src/StaffBundle/Controller/EmployeeController.php
@@ -3,9 +3,11 @@
 namespace StaffBundle\Controller;
 
 use AppBundle\Controller\RestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 class EmployeeController extends RestController
@@ -25,5 +27,20 @@ class EmployeeController extends RestController
         }
 
         return new JsonResponse($result);
+    }
+
+    /**
+     * @Route("/employees/{id}")
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     */
+    public function getAction(Request $request, $id)
+    {
+        if ($employee = $this->getDoctrine()->getRepository('StaffBundle\Entity\Employee')->find($id)) {
+            return new JsonResponse($employee->toArray());
+        }
+
+        throw new NotFoundHttpException('Employee not found.');
     }
 }
